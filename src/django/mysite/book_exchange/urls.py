2d122f18fcc616e14311b3da5993251cb0e6c86d
Adrian <32913236+adrigski@users.noreply.github.com>
2022-11-15 18:40:47 -0600

    Added ability for book listing to appear on buy page

--- src/django/mysite/book_exchange/urls.py
+++ src/django/mysite/book_exchange/urls.py
@@ -1,15 +1,34 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
 
 from . import views
 
+
 app_name = 'book_exchange'
+
+router = routers.DefaultRouter()
+router.register(r'school', views.SchoolViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'book', views.BookViewSet)
+router.register(r'book_for_sale', views.BookForSaleViewSet)
+router.register(r'pinned_book', views.PinnedBookViewSet)
+router.register(r'transaction', views.TransactionViewSet)
+router.register(r'school_uses_book', views.SchoolUsesBookViewSet)
+
+
+
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
+    path('api/', include(router.urls)),
+    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('<int:pk>/', views.MainView.as_view(), name='main'),
-    path('<int:pk>/buy/', views.BuyView.as_view(), name='buy'),
+    path('<int:pk>/buy/', views.browse_books, name='browse_books'),
     path('<int:pk>/sell/', views.SellView.as_view(), name='sell'),
     path('<int:pk>/profile/', views.ProfileView.as_view(), name='profile'),
     path('<user_id>/buy/<book_for_sale_id>/', views.book_info, name='book_info'),
+
     #path('<user_id>/buy/<book_id>', , name='sell'),
     #path('<user_id>/sell/', views.SellView.as_view(), name='sell'),
 ]
